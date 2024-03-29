d93f377bf2eeba7d14f970a76f249a30ed3be1e3
Tainá dos Santos <taina.santoss@eaportal.org>
2024-03-29 01:56:13 -0300

    quinto commit do app

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import iconLuxury from "./assets/icon-luxury.svg"
 
 export default function App(){
   return(
-    <main>
+    <main className="min-h-svh bg-offwhitebg py-20 px-6">
       <section>
         <div className="bg-orange">
         <img src={iconSedans} alt="icone carros Sedans" />
