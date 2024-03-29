c8f2d0f9a0192734909cc804ef97ade9792f03e8
Tainá dos Santos <taina.santoss@eaportal.org>
2024-03-29 01:50:12 -0300

    quarto commit do app

--- src/App.jsx
+++ src/App.jsx
@@ -9,25 +9,25 @@ export default function App(){
       <section>
         <div className="bg-orange">
         <img src={iconSedans} alt="icone carros Sedans" />
-        <h2>Sedans</h2>
-        <p>Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
-        <a href="https://www.webmotors.com.br/sedans"
+        <h2 className="font-shoulders">Sedans</h2>
+        <p className="font-lexend">Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
+        <a className="font-lexend" href="https://www.webmotors.com.br/sedans"
         target="_blank">Ver mais</a>
         </div>
 
         <div className="bg-green">
         <img src={iconSuvs} alt="icone carros SUV" />
-        <h2>SUVs</h2>
-        <p> Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road.</p>
-        <a href="https://www.webmotors.com.br/suvs"
+        <h2 className="font-shoulders">SUVs</h2>
+        <p className="font-lexend"> Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road.</p>
+        <a className="font-lexend" href="https://www.webmotors.com.br/suvs"
         target="_blank">Ver mais</a>
         </div>
 
         <div className="bg-dark-green">
         <img src={iconLuxury} alt="icone carros de luxo" />
-        <h2>Luxuosos</h2>
-        <p> Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
-        <a href="https://www.webmotors.com.br/carros-de-luxo"
+        <h2 className="font-shoulders">Luxuosos</h2>
+        <p className="font-lexend"> Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
+        <a className="font-lexend" href="https://www.webmotors.com.br/carros-de-luxo"
         target="_blank">Ver mais</a>
         </div>
       </section>
