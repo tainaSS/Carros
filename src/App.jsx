f3235702a799e52e0d5a10e18dedd8ce7b2190a9
Tainá dos Santos <taina.santoss@eaportal.org>
2024-03-29 02:51:50 -0300

    projeto finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,13 @@ export default function App(){
   return(
     <main className="min-h-svh bg-offwhitebg py-20 px-6 antialiased 
     lg:flex justify-center items-center" >
-      <section className="lg:flex max-w-[920px]">
-        
+      <div className="lg:flex max-w-[920px]">
+
         <Sedans/>
         <Suvs />
         <Luxuosos />
 
-      </section>
+      </div>
     </main>
   )
 }
